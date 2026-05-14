65d09b77676f096d63db6565b904d1e919f7d904
deepikagrawall <99550087+deepikagrawall@users.noreply.github.com>
2026-05-14 16:01:24 +0530

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { Admin } from './components/Admin.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
